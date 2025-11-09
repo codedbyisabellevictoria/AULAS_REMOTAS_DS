d71b54fb9ce9068f104b0dcb1302616384f4d651
Isabelle Victória <isabellevictoria.sq@gmail.com>
2025-11-08 23:34:24 -0300

    realizado

---
+++ atividade01/at1.py
@@ -0,0 +1,7 @@
+nome = str(input('Qual o seu nome?: '))
+idade = int(input('Qual o sua idade?: '))
+
+if age >= 18:
+    print(f"Ola {nome}, voce e maior de idade!")
+else:
+    print(f"Ola {nome}, voce e menor de idade!")
